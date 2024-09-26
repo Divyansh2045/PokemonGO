ea459ff1c12fd0c61c5c7bece358f99f99cd335d
Divyansh2045 <divyanshnagar@hotmail.com>
2024-09-27 00:43:31 +0100

    created project files and main.cpp

---
+++ PokemonGO/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string playerName;
+	cout << "Enter your name champion" << endl;
+	cin >> playerName;
+	cout << "Great start " << playerName << endl;
+
+	return 0;
+}
