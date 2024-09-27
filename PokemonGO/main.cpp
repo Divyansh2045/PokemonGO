c76af82fc7532bbbdef120a999a97b35d43186df
Divyansh2045 <divyanshnagar@hotmail.com>
2024-09-27 16:20:33 +0100

    pokemon choice made created switch statement

--- PokemonGO/main.cpp
+++ PokemonGO/main.cpp
@@ -10,32 +10,38 @@ int main()
 
 	cout << "Hi, I am Professor Oak, and this is Pokemon Arena" << endl;
 	cout << "I give you the choice of three pokemons from which you can select only 1 to begin your journey to CATCH'EM ALL" << endl;
-	cout << "Your first option is 1.Pikachu" << endl;
+	cout << "Your first option is 1.Bulbasaur" << endl;
 	cout << "2. turtle " << endl;
 	cout << "and lastly 3.charizard" << endl;
 
 	cout << "Which Pokemon would you like to choose. Enter the number" << endl;
 
-	int choice;
-	cin >> choice;
-	if (choice ==  1)
+
+
+	int chosenPokemon;
+	cin >> chosenPokemon;
+	switch (chosenPokemon)
 	{
-		cout << "You have chosen Pikachu.. Well done!!" << endl;
+	case 1:
+
+		cout << "Well done on choosing Bulbasaur, must say an excellent choice" << endl;
+		break;
+
+	case 2:
+		cout << "Well done on choosing Turtle, must say an excellent choice" << endl;
+		break;
+
+	case 3:
+		cout << "Well done on choosing charizard, must say an excellent choice" << endl;
+		break;
+	default:
+		cout << "You have chosen Pikachu, well done now on you go" << endl;
+		break;
+
 	}
-	else if (choice == 2)
-	{
-		cout << "You have chosen Bulbasaur... Well done!!" << endl;
-	}
-	else if (choice == 3)
-	{
-		cout << "Charizard... hmmm... an excellent choice.. well done" << endl;
-	}
-	else
-	{
-		cout << "\nInvalid Input!  Please restart the game" << endl;
-	}
-	cout<<"But beware, Trainer, this is only the beginning."<<endl; 
-	cout << "Your journey is about to unfold.Now let’s see if you’ve got what it takes to keep going!\nGood luck, and remember… Choose wisely!" << endl;
+
+	cout << "But beware, Trainer, this is only the beginning." << endl;
+	cout << "Your journey is about to unfold.Now let us see if you have got what it takes to keep going!\nGood luck, and remember… Choose wisely!" << endl;
 
 	return 0;
 
